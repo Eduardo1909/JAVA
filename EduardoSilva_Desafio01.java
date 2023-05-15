1326acf06c660fc1df81503396c2911a830ee767
Eduardo1909 <131814076+Eduardo1909@users.noreply.github.com>
2023-05-15 16:58:54 -0300

    Desafio java01
    Java que mostra números de 0 até 1000

---
+++ EduardoSilva_Desafio01.java
@@ -0,0 +1,15 @@
+/* 
+Descrição: JAVA que mostre números de 0 até 1000
+Nome do autor: Eduardo E. C. Silva
+Data da última atualização: 15/05/23
+*/
+
+public class EduardoSilva_Desafio01{
+    public static void main(String[]args) {
+    int num = 0;
+
+    while(num <= 1000) {
+        System.out.println(num++);
+    }
+}
+}
