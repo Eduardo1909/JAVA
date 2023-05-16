820f3231a5cf499561d8f6ebdc7add98aa35ae77
Eduardo1909 <131814076+Eduardo1909@users.noreply.github.com>
2023-05-16 17:09:42 -0300

    Eduardo_Exercicio01

---
+++ Eduardo_Exercicio01.java
@@ -0,0 +1,19 @@
+/* Descrição: Algoritmo que captura um valor inserido pelo usuário e desconte 15% dp valor inserido
+ * Autor: Eduardo Silva
+ * Ultima edição: 16/05/2023
+ */
+
+import java.util.Scanner;
+
+public class Eduardo_Exercicio01 {
+    public static void main(String[]args)
+    Scanner ler = new Scanner(System.in);
+    double n1 = 0.0;
+
+    System.out.print("Digite o valor que deseje aplicar o desconto: ");
+    n1 = ler.nextDouble();
+    
+    System.out.print("O valor com desconto é:" +(n1-((n1/100)*15));
+}
+    
+}
