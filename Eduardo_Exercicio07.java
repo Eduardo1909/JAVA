385415e36b8690b288338dc168bdad0cefac1153
Eduardo1909 <131814076+Eduardo1909@users.noreply.github.com>
2023-05-17 17:49:48 -0300

    Eduardo_Exercicio07

---
+++ Eduardo_Exercicio07.java
@@ -0,0 +1,26 @@
+/* Descrição : Java para a compra da senhora Jumiralds
+ * Autor : Eduardo
+ * Ultima atualização : 17/05/2023 [] 05/17/2023
+ */
+
+ import java.util.Scanner;
+
+public class Eduardo_Exercicio07 {
+    public static void main(String[]args)
+    Scanner ler = new Scanner(System.in);
+    int a = 0;
+
+    System.out.print("Digite um número aleatorio: ");
+    a = ler.nextInt();
+
+    //SE e SE NÃO
+    if(a = 654655){
+    System.out.print("Acerto mizeravi!")
+    }
+    else if(a != 654655){
+    System.out.print("Erro CORNO!! AHUAHUAHUAHUA")
+    }
+}
+
+    
+}
