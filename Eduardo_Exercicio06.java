967a905d65131535b1d9eb190f5ec7c0166612b0
Eduardo1909 <131814076+Eduardo1909@users.noreply.github.com>
2023-05-17 17:06:21 -0300

    Eduardo_Exercicio06

---
+++ Eduardo_Exercicio06.java
@@ -0,0 +1,26 @@
+/* Descrição : Java para a compra da senhora Jumiralds
+ * Autor : Eduardo
+ * Ultima atualização : 17/05/2023 [] 05/17/2023
+ */
+
+ import java.util.Scanner;
+
+public class Eduardo_Exercicio06 {
+    public static void main(String[]args)
+    Scanner ler = new Scanner(System.in);
+    int k = 0;
+    int l = 0;
+    int r = 0;
+
+    System.out.print("Digite a quantia de sorvetes de Kiwi, Laranja e Romã qua a pessoa comprou: ");
+    k = ler.nextDouble();
+    l = ler.nextDouble();
+    r = ler.nextDouble();
+
+    System.out.print("O valor que deu foi de: R$");
+    System.out.print((k*3.2)+(l*2.39)+(r*7.99));
+
+}
+
+    
+}
