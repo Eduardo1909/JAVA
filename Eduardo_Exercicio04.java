1579f43c8e2a4fef8b8900dc7a062050d4303d90
Eduardo1909 <131814076+Eduardo1909@users.noreply.github.com>
2023-05-17 16:35:22 -0300

    Eduardo_Exercicio04

---
+++ Eduardo_Exercicio04.java
@@ -0,0 +1,30 @@
+/* Descrição : Java que inverte valores
+ * Autor : Eduardo
+ * Ultima atualização : 17/05/2023 [] 05/17/2023
+ */
+
+ import java.util.Scanner;
+
+public class Eduardo_Exercicio04 {
+    public static void main(String[]args)
+    Scanner ler = new Scanner(System.in);
+    int a = 0;
+    int b = 0;
+    int c = 0;
+
+    System.out.print("Digite o valor de A: ");
+    a = ler.nextDouble();
+    System.out.print("Digite o valor de B: ");
+    b = ler.nextDouble();
+
+    c = a;
+    a = b;
+    b = c;
+
+    System.out.print("Este é o valor de A e em seguida o de B:");
+    System.out.print(a);
+    System.out.print(b);
+
+}
+
+}
