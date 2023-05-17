ad4fe878f3da79cf6f0f307c2ceffe0877ecf0b4
Eduardo1909 <131814076+Eduardo1909@users.noreply.github.com>
2023-05-17 16:43:39 -0300

    Eduardo_Exercicio05

---
+++ Eduardo_Exercicio05.java
@@ -0,0 +1,25 @@
+/* Descrição : Java que mostra o antecessor e sucessor de um número digitado pelo usuário
+ * Autor : Eduardo
+ * Ultima atualização: 17/05/2023 [] 05/17/2023
+ */
+
+import java.util.Scanner;
+
+public class Eduardo_Exercicio05 {
+    public static void main(String[]args)
+    Scanner ler = new Scanner(System.in);
+    int a = 0;
+
+    System.out.print("Digite um valor: ");
+    a = ler.nextDouble();
+
+    System.out.print("Este é seu antecessor, em seguida o que você digitou e por fim seu sucessor:");
+    System.out.print(a-1);
+    System.out.print(a);
+    System.out.print(a+1);
+
+}
+
+}
+
+}
