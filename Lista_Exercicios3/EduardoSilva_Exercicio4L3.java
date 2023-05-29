0ec9351d74f0074c32ebf55dfaafc353c8de48ca
Eduardo1909 <131814076+Eduardo1909@users.noreply.github.com>
2023-05-29 17:43:13 -0300

    EduardoSilva_Exercicio3L4

---
+++ Lista_Exercicios3/EduardoSilva_Exercicio4L3.java
@@ -0,0 +1,22 @@
+/*Descrição: JAVA que mostra os números entre dois outros números
+*Autor: Eduardo
+*Ultima atualização: 29/05/23
+*/
+
+import java.util.Scanner;
+
+public class EduardoSilva_Exercicio4L3 {
+    public static void main(String[]args) {
+
+//DECLARAÇÃO DE VARIAVEIS
+    Scanner ler = new Scanner(System.in);
+    int num = 0;
+
+    while(num <= 1000) {
+        System.out.println(num++);
+        if (num % 2 = 1){
+            System.out.println(num);
+}    
+}
+}
+}
