75822b866014d793a1f41667c0f8c04e43ecc8a8
Eduardo1909 <131814076+Eduardo1909@users.noreply.github.com>
2023-05-29 15:49:10 -0300

    Update Eduardo_Exercicio02.java

--- Lista_Exercicios2/Eduardo_Exercicio02.java
+++ Lista_Exercicios2/Eduardo_Exercicio02.java
@@ -10,16 +10,20 @@ import javax.lang.model.util.ElementScanner14;
 public class Eduardo_Exercicio02 {
     
     public static void main (String[]args)
+
+//DECLARAÇÃO DE VARIAVEL        
     Scanner ler = new Scanner (System.in)
     int n1 = 0;
     int n2 = 0;
     int n3 = 0;
 
+//INICIO    
     System.out.print("Digite três números: ");
     n1 = ler.nextdouble();
     n2 = ler.nextdouble();
     n3 = ler.nextdouble();
 
+//IF e ELSE    
     if(n1>n2 && n1>n3)
     {}
     else {
