6809ed8767ec6e7586dbbfd245dd348ca20d4f68
Eduardo1909 <131814076+Eduardo1909@users.noreply.github.com>
2023-05-29 18:47:48 -0300

    EduardoSilva_Exercicio6L4

---
+++ Lista_Exercicios4/EduardoSilva_Exercicio6L4.java
@@ -0,0 +1,20 @@
+/*
+Descrição: JAVA
+Nome: Eduardo
+Data: 28/05/2023
+ */
+
+ import java.util.Scanner;
+ 
+ public class EduardoSilva_Exercicio6L4 {
+      public static void main(String[] args) {
+ //VARIAVEL
+ Scanner ler = new Scanner(System.in);
+ for (int i = 10; i >= 0; i--) {
+     
+     System.out.println(i);
+ }
+ 
+ System.out.println("fim");
+ }
+ }
