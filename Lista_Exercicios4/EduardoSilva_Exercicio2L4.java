06bdb961774608bfe04875c363393250a5ec1db4
Eduardo1909 <131814076+Eduardo1909@users.noreply.github.com>
2023-05-29 18:16:40 -0300

    EduardoSsilva_Exercicio2L4

---
+++ Lista_Exercicios4/EduardoSilva_Exercicio2L4.java
@@ -0,0 +1,28 @@
+/*
+Descrição: Esta lógica recebe três números, os soma e caso a soma seja diferente de 48 ou 71, ele se repete INFINITAMENTE
+Nome: Alan Santana Leão
+Data: 28/05/2023
+ */
+
+ import java.util.Scanner;
+ 
+ public class EduardoSilva_Exercicio2L4 {
+     public static void main(String[] args) {
+//VARIAVEIS
+        Scanner scanner = new Scanner(System.in);
+        int soma = 0;
+ 
+//INICIO        
+         while (soma != 48 && soma != 71) {
+             System.out.println("Digite três números: ");
+ 
+             int numero1 = scanner.nextInt();
+             int numero2 = scanner.nextInt();
+             int numero3 = scanner.nextInt();
+ 
+             soma = numero1 + numero2 + numero3;
+         }
+ 
+         System.out.println("A soma é igual a 48 ou 71.");
+     }
+} 
