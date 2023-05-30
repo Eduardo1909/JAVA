565a58953e8d953950fd204df4feb1cf93e32cba
Eduardo1909 <131814076+Eduardo1909@users.noreply.github.com>
2023-05-30 17:36:13 -0300

    Update EduardoSilva_Exercicio3L4.java

--- Lista_Exercicios4/EduardoSilva_Exercicio3L4.java
+++ Lista_Exercicios4/EduardoSilva_Exercicio3L4.java
@@ -1,9 +1,9 @@
-/*Descrição: JAVA 
+/*Descrição: JAVA que soma números inteiros e reais aleatorios e ve qual é maior
 *Nome: Eduardo
 *Data: 29/05/2023
  */
 
- import java.util.Random;
+ import java.util.Random; //programa random
  
 public class EduardoSilva_Exercicio3L4 {
      public static void main(String[] args) {
@@ -12,7 +12,7 @@ public class EduardoSilva_Exercicio3L4 {
          int si = 0;
 
 //GERAÇÃO DOS NÚMEROS INTEIROS        
-         Random randomInt = new Random();
+         Random rInt = new Random();
          for (int i = 0; i < qn; i++) {
              int ni = randomInt.nextInt();
              si += ni;
@@ -31,11 +31,11 @@ public class EduardoSilva_Exercicio3L4 {
          System.out.println("A soma dos números reais é: " + sr);
  
          if (si > sr) {
-             System.out.println("A soma dos números inteiros é maior.");
+             System.out.println("A soma dos números inteiros é maior."); //Se a soma dos inteiros for maior que os reais
          } else if (si < sr) {
-             System.out.println("A soma dos números reais é maior.");
+             System.out.println("A soma dos números reais é maior."); //Se a soma dos reais for maior que os inteiros
          } else {
-             System.out.println("As somas dos números inteiros e reais são iguais.");
+             System.out.println("As somas dos números inteiros e reais são iguais."); //Se a soma de ambos for igual
         }
     }
 }
