b362d18e948ecee24d6d576eee3d1239ff95f3c4
Eduardo1909 <131814076+Eduardo1909@users.noreply.github.com>
2023-05-30 17:40:47 -0300

    Update EduardoSilva_Exercicio5L4.java

--- Lista_Exercicios4/EduardoSilva_Exercicio5L4.java
+++ Lista_Exercicios4/EduardoSilva_Exercicio5L4.java
@@ -9,12 +9,12 @@ import java.util.Scanner;
 public class EduardoSilva_Exercicio5L4 {
      public static void main(String[] args) {
 //VARIAVEL
-Scanner ler = new Scanner(System.in);
+Scanner ler = new Scanner(System.in); //declaração scanner
 for (int i = 10; i >= 0; i--) {
     
     System.out.println(i);
 }
 
-System.out.println("fim");
+System.out.println("fim"); //O FIM!!!! PAPAPAAAAAAAAAAAAAAAAAAAAAA
 }
 }
