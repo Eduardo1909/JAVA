e1983d7c005a7b1f62714716eecba5e71df8258f
Eduardo1909 <131814076+Eduardo1909@users.noreply.github.com>
2023-05-30 17:39:03 -0300

    Update EduardoSilva_Exercicio4L4.java

--- Lista_Exercicios4/EduardoSilva_Exercicio4L4.java
+++ Lista_Exercicios4/EduardoSilva_Exercicio4L4.java
@@ -1,14 +1,13 @@
-/*Descrição: JAVA 
+/*Descrição: JAVA que mostra a quantia de digitos tem a palavra
 *Nome: Eduardo
 *Data: 29/05/2023
  */
 
- import java.util.Random;
- import java.util.Scanner;
+import java.util.Scanner; //programa Scanner
 
 public class EduardoSilva_Exercicio4L4 {
             public static void main(String[] args) {
-                Scanner ler = new Scanner(System.in);
+                Scanner ler = new Scanner(System.in); //declaração do scanner
         
                 System.out.print("Digite um texto: ");
                 String texto = ler.nextLine();
