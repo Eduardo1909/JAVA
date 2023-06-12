d7df008906162a7265f15ebb0fd8d085ea60f454
Eduardo1909 <131814076+Eduardo1909@users.noreply.github.com>
2023-06-12 17:43:16 -0300

    OrObj_Ex5

---
+++ OrObj5.java
@@ -0,0 +1,37 @@
+/*Descrição: Crie uma classe e atribua a ela, no mínimo, 10 atributos, 10 métodos a 1 método construtor.
+*Autor: Eduardo Enrique
+*Ultima atualização: 07/06/2023
+*/
+
+public class OrObj5{
+    public static void main (String[]args)   
+}
+
+class celular{
+//Atributos
+/*1*/
+/*2*/
+/*3*/
+/*4*/
+/*5*/
+/*6*/
+/*7*/
+/*8*/
+/*9*/
+/*10*/
+
+//Métodos
+/*1*/
+/*2*/
+/*3*/
+/*4*/
+/*5*/
+/*6*/
+/*7*/
+/*8*/
+/*9*/
+/*10*/
+
+//Método construtivo
+/*1*/
+}
